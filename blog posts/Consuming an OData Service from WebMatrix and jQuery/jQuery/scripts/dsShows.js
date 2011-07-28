830182c6f253527d35122a5c58606790686a2044
Clark Sell <clark.sell@csell.net>
2011-07-28 12:32:20 -0500

    inital checking to support the blog post Consuming an OData Serice from WebMatrix and jQuery

---
+++ blog posts/Consuming an OData Service from WebMatrix and jQuery/jQuery/scripts/dsShows.js
@@ -0,0 +1,20 @@
+﻿jQuery(document).ready(function () {
+    var query = "http://developersmackdown.com/services/odata/Shows" //root uri
+    + "?$select=ShowId,Title,PublicUri" //reduce the payload to what we need
+    + "&$orderby=ShowId%20desc" //order by and sort by the latest shows
+    + "&$top=5" //take the top 5
+    + "&$format=json" //give me json
+    + "&$callback=callback"; //this is my callback
+   
+     jQuery.ajax({
+        dataType: 'jsonp',
+        url: query,
+        jsonpCallback: 'callback',
+        success: callback
+    });
+
+    function callback(result) {
+        var shows = result.d;
+        jQuery('#dsListTemplate').tmpl(shows).appendTo('#ds_shows');
+    }
+});
